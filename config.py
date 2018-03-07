7480e52e3049c102cccde31adf0afb18471ad024
Gregory Maurer <gregmaurer@gmail.com>
2018-03-06 17:13:46 -0700

    new data directory parsing

--- config.py
+++ config.py
@@ -33,30 +33,39 @@ if conf_flag:
     # NOTE - If base_path is unset in conf, all other paths must be complete
     base_path = conf['base_path']
 
-    #config_path_user = os.path.join(base_path,
-    #        conf['paths']['config'])
-    raw_in_path = os.path.join(base_path,
-            conf['default_data_paths']['raw_in'])
-    raw_bak_path = os.path.join(base_path,
-            conf['default_data_paths']['raw_bak'])
-    qa_path = os.path.join(base_path,
-            conf['default_data_paths']['qa'])
+    # Get 5 default paths from yaml config,
+    # First clean out None values from the default path dict
+    defpaths = {k:v for k,v in conf['default_data_paths'].items()
+            if v is not None}
+    # The first 2 are required (key error if missing) and the rest are set
+    # to match qa path by default
+    defpaths['raw_in'] = os.path.join(base_path, defpaths['raw_in'])
+    defpaths['qa'] = os.path.join(base_path, defpaths['qa'])
+    defpaths['raw_bak'] = os.path.join(base_path,
+            defpaths.get('raw_bak', defpaths['qa']))
+    defpaths['raw_std'] = os.path.join(base_path,
+            defpaths.get('raw_std', defpaths['qa']))
+    defpaths['user'] = os.path.join(base_path,
+            defpaths.get('user', defpaths['qa']))
+    # Complete the user paths dictionary
+    userpaths = {k:v for k,v in conf['user_subdirs'].items() if v is not None}
+    for k in userpaths.keys():
+        userpaths[k] = os.path.join(defpaths['user'], '{SITE}', k)
+    # Complete the default paths in a new dictionary
+    datapaths = {}
+    for k in ['raw_in', 'qa', 'raw_bak', 'raw_std']:
+        datapaths[k] = os.path.join(defpaths[k], '{SITE}', k)
+    # Merge the  default and user path dictionaries
+    datapaths.update(userpaths)
     
-    datalog_py_path = os.path.join(base_path,
-            conf['datalog_py'])
+    # Datalog code path
+    datalog_py_path = os.path.join(base_path, conf['datalog_py'])
 
     # Site metadata file
-    sitedata_file = os.path.join(base_path,
-            conf['site_metadata'])
-
-    # Get name and subdirectory for datalog data types
-    datasubdirs = {'rawdata_incoming':'',  
-        'rawdata_backup': 'raw_bak/',
-        'rawdata_standardized': 'raw_std/',
-        'quality_assured': 'qa/'}
-
-    datadirs = list(datasubdirs.keys())
-
+    sitedata_file = os.path.join(base_path, conf['site_metadata'])
+    
+    # Print available data subdirectories for user:
+    datadirs = list(datapaths.keys())
     print('Each site has {0} data directories available: \n {1} \n'
             'Use "iodat.site_datadir(sitename, datadir=datadir_name)"'
             'to get proper path'.format(len(datadirs), ', '.join(datadirs)))
