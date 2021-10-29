a35bf7f448215fc6b3c38f0d66dd1bedba288876
Gregory E. Maurer <gregmaurer@gmail.com>
2021-10-29 17:26:41 -0600

    minor setup

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='sawyer',
-    description='manage and quality-assure data from dataloggers and environmental sensor networks',
+    description='Manage and quality-assure data from dataloggers and environmental sensor networks',
     long_description=long_description,
     version='2021.1b1',
     url='https://github.com/gremau/sawyer',  # Optional
     author='Gregory E. Maurer',  # Optional
-    author_email='gmaurer@nmsu.edu',  # Optional
+    author_email='gmaurer@nmsu.edu',  # Optional
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     install_requires=[
